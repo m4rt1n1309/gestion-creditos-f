b80109bca2e6e32a553119c292e7119c55ffc972
dantegambarte <da.nt3@hotmail.com>
2026-04-16 01:27:28 -0300

    feat: update app structure with routing and authentication guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { AsyncPipe } from '@angular/common';
+import { MockAuthService } from './core/auth/mock-auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gestion-creditos-f';
+  constructor(public auth: MockAuthService) {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+import { roleGuard } from './core/auth/role.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/public/login/login.routes').then(
+        (r) => r.LOGIN_ROUTES,
+      ),
+  },
+  {
+    path: 'admin',
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['ADMIN'] },
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then((r) => r.ADMIN_ROUTES),
+  },
+  {
+    path: 'seller',
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['SELLER', 'ADMIN'] },
+    loadChildren: () =>
+      import('./features/seller/seller.routes').then((r) => r.SELLER_ROUTES),
+  },
+  {
+    path: 'collector',
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['COLLECTOR'] },
+    loadChildren: () =>
+      import('./features/collector/collector.routes').then(
+        (r) => r.COLLECTOR_ROUTES,
+      ),
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+];
