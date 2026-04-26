f9f72c6247ccc11b64de482c44a37e31bfd0b91d
dantegambarte <da.nt3@hotmail.com>
2026-04-26 03:15:09 -0300

    feat(tests): add router and mock auth service to AppComponent tests; remove unused approvals mock data

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+﻿import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { MockAuthService } from './core/auth/mock-auth.service';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter([]), MockAuthService],
     }).compileComponents();
   });
 
